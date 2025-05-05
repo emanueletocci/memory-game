f71792aac9ab667ebc5616380694a480ce606941
emanueletocci <tocciemanuele03@gmail.com>
2025-05-05 13:35:24 +0200

    test update

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
 
+// Enrtypoint - prende in input un widget che viene inserito come root nel widget tree
 void main() {
   runApp(const MainApp());
 }
 
+// root del widget tree
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // centra tutto verticalmente
+            children: [
+              Text('Hello World!'),
+              SizedBox(height: 20), // spazio tra testo e immagine
+              Image.network(
+                'https://images.unsplash.com/photo-1728044849316-b42a4b25e507?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                width: 200,
+                height: 200,
+              ),
+            ],
+          ),
         ),
       ),
     );
